4dbacff25ccaf3ffe00fd5a2d37e89bafb336993
Ali Aghajani <105543467+peymangiit@users.noreply.github.com>
2023-09-12 16:49:25 +0330

    add message for ending

--- script.js
+++ script.js
@@ -11,13 +11,11 @@ const diceRolls = [
     "&#9860;",
     "&#9861;",
 ];
-var n=1;
+var n = 1;
 var droll;
 function getDiceFace() {
     let result = Math.ceil(Math.random() * 6); // give a number between 1-6
     droll = result;
-    console.log(typeof(droll));
-    // console.log(droll);
     return diceRolls[result];
 }
 
@@ -27,71 +25,72 @@ buttonEl.addEventListener("click", () => {
         diceEl.classList.remove("roll-animation");
         diceEl.innerHTML = `<div class="dice" id="dice">${getDiceFace()}</div>`;
         updatePawn();
-       
-    },1000);
-    
+    }, 1000);
 });
 
-
- function updatePawn(){
-   let stopper= n+droll;
-   if(stopper <= 43){
-         var timer = setInterval(()=>{
-        document.querySelector(`#box${++n}`).appendChild(pawn);
-        if(n>=stopper){ stopTimer()}
-        },100);
-        function stopTimer(){
+function updatePawn() {
+    if (n + droll <= 42) {
+        var stopper = n + droll;
+        var timer = setInterval(() => {
+            document.querySelector(`#box${++n}`).appendChild(pawn);
+            if (n >= stopper) {
+                stopTimer();
+            }
+        }, 100);
+        function stopTimer() {
             clearInterval(timer);
         }
-        console.log("stoper"+stopper);
-        console.log("n is:"+n);
+        if (stopper == 42) {
+            //alert and reset game
+            setTimeout(() => {
+                alert("Congratulation on finishing, let's play again!");
+                document.querySelector("#box1").appendChild(pawn);
+                n = 1;
+            }, 500);
+        }
+    } else if (n + droll > 42) {
+        setTimeout(() => {
+            alert("Dice roll is TOO BIG ");
+        }, 200);
     }
 
-    if(stopper==3){
-        console.log("ladder up")
-        setTimeout(()=>{
-                    document.querySelector("#box14").appendChild(pawn);
-                   n=14;
-        } ,500);
+    //ladders
+    if (stopper == 3) {
+        setTimeout(() => {
+            document.querySelector("#box14").appendChild(pawn);
+            n = 14;
+        }, 500);
     }
-    if(stopper==13){
-        setTimeout(()=>{
+
+    if (stopper == 13) {
+        setTimeout(() => {
             document.querySelector("#box26").appendChild(pawn);
-           n=26;
-        } ,1000);
+            n = 26;
+        }, 1000);
     }
-    if(stopper==19){
-        setTimeout(()=>{
+    if (stopper == 19) {
+        setTimeout(() => {
             document.querySelector("#box32").appendChild(pawn);
-           n=32;
-        } ,1000);
+            n = 32;
+        }, 1000);
     }
     //snakes bit action
-    if(stopper==16){
-        setTimeout(()=>{
+    if (stopper == 16) {
+        setTimeout(() => {
             document.querySelector("#box4").appendChild(pawn);
-           n=4;
-        } ,1000);
+            n = 4;
+        }, 1000);
     }
-    if(stopper==28){
-        setTimeout(()=>{
+    if (stopper == 28) {
+        setTimeout(() => {
             document.querySelector("#box2").appendChild(pawn);
-           n=2;
-        } ,1000);
+            n = 2;
+        }, 1000);
     }
-    if(stopper==38){
-        setTimeout(()=>{
+    if (stopper == 38) {
+        setTimeout(() => {
             document.querySelector("#box21").appendChild(pawn);
-           n=21;
-        } ,1000);
+            n = 21;
+        }, 1000);
     }
 }
-
-
-// console.log
-// if(updatePawn == 3){
-//     console.log("compare")
-//     setTimeout(()=>{
-//         document.querySelector("#box14").appendChild(pawn);
-//     } ,100);
-// }
